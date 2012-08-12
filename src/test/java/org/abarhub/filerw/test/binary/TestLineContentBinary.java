2d944c16d20d5995021bf802a07aa465e9163648
abarhub <abarhub@github.com>
2012-08-12 13:34:15 +0200

    tests

--- src/test/java/org/abarhub/filerw/test/binary/TestLineContentBinary.java
+++ src/test/java/org/abarhub/filerw/test/binary/TestLineContentBinary.java
@@ -9,12 +9,43 @@ public class TestLineContentBinary {
 
 	@Test
 	public void test() {
-		byte[] tab={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+		byte[] tab={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
 		LineContentBinary<FieldsListChamps2> line;
 		line=new LineContentBinary<FieldsListChamps2>(FieldsListChamps2.class, tab);
-		assertEquals(FieldsListChamps2.Code1.getLength(),line.get(FieldsListChamps2.Code1));
-		assertEquals(FieldsListChamps2.Code2.getLength(),line.get(FieldsListChamps2.Code2));
-		assertEquals(FieldsListChamps2.Code3.getLength(),line.get(FieldsListChamps2.Code3));
+		assertEquals(FieldsListChamps2.Code1.getLength(),size(line.get(FieldsListChamps2.Code1)));
+		assertEquals(FieldsListChamps2.Code2.getLength(),size(line.get(FieldsListChamps2.Code2)));
+		assertEquals(FieldsListChamps2.Code3.getLength(),size(line.get(FieldsListChamps2.Code3)));
+		
+		assertTrue(equals(new byte[]{1,2,3,4},line.get(FieldsListChamps2.Code1)));
+		assertTrue(equals(new byte[]{5,6,7,8,9,10},line.get(FieldsListChamps2.Code2)));
+		assertTrue(equals(new byte[]{11,12,13,14,15,16,17},line.get(FieldsListChamps2.Code3)));
 	}
 
+	private int size(byte[] tab)
+	{
+		if(tab==null)
+			return 0;
+		else
+			return tab.length;
+	}
+	
+	private boolean equals(byte[] tab,byte[] tab2)
+	{
+		if(tab==null)
+			return tab2==null;
+		else
+		{
+			if(tab2==null)
+				return false;
+			if(tab.length!=tab2.length)
+				return false;
+			for(int i=0;i<tab.length;i++)
+			{
+				if(tab[i]!=tab2[i])
+					return false;
+			}
+			return true;
+		}
+	}
+	
 }
