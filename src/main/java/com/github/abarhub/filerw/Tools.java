f9b7c7d8f7bbcebbb7b6020b19ed90dc8fa42801
abarhub <abarhub@github.com>
2014-01-12 15:46:14 +0100

    Amélioration de la licence

--- src/main/java/com/github/abarhub/filerw/Tools.java
+++ src/main/java/com/github/abarhub/filerw/Tools.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2012-2014. Alain Barret
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.abarhub.filerw;
 
 import java.util.ArrayList;
@@ -7,7 +23,7 @@ import java.util.List;
 public final class Tools {
 
 	private Tools() {
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor
 	}
 
 	public static <T extends Field> int getSize(Class<T> clazz) {
